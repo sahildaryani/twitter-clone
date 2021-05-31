bb242b505bb749876dd131a9bb8349814399277c
sahildaryani <sahil.daryani@gmail.com>
2021-05-31 23:01:14 +0530

    sidebar complete

--- twitter/src/App.js
+++ twitter/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     //BEM
     <div className="app">
       <h1>LEss GOO bois</h1>
+      {/* Sidebar */}
+      <Sidebar />
+      {/* FEED */}
+
+      {/* WIDGETS */}
     </div>
   );
 }
